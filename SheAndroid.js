0733579c7c21545248ae9e13608bef3ae5fcdd92
sym-crypto <78688194+sym-crypto@users.noreply.github.com>
2024-03-20 12:32:30 +0100

    Create SheAndroid.js
    a shell for web app

---
+++ SheAndroid.js
@@ -0,0 +1,18 @@
+/// must install exec module
+/// npm install exec
+const { exec } = require('child_process');
+
+var cmdSH;
+
+while(true) {
+  cmdSH = prompt('AppShell/');
+  exec(cmdSH, (error, stdout, stderr) => {
+    if (error) {
+      console.error(`exec error: ${error}`);
+      return;
+    }
+
+    console.log(`stdout: ${stdout}`);
+    console.error(`stderr: ${stderr}`);
+   });
+}
